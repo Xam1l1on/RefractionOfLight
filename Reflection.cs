a2fb02bb67244c5f0601677074e014af6650c456
Xam1l1on <kir.solomatin@gmail.com>
2020-09-12 12:12:40 +0300

    Добавьте файлы проекта.

---
+++ Reflection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefractionOfLight
+{
+    class Reflection
+    {
+        public double AngleOfReflection(double angleOfInc)
+        {
+            double angleOfReflection = angleOfInc;
+            return angleOfReflection;
+        }
+    }
+}
---
+++ Refraction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefractionOfLight
+{
+    class Refraction
+    {
+        private double angleOfRefraction;
+        private double indicesOfRefraction;
+
+        public double AngleOfRefraction(double oneMedia, double twoMedia, double angleOfInc)
+        {
+            indicesOfRefraction = oneMedia / twoMedia;
+            if (oneMedia == twoMedia)
+            {
+                angleOfRefraction = angleOfInc;
+            }
+            else if (oneMedia < twoMedia)
+            {
+                angleOfRefraction = Math.Sin(angleOfInc) * indicesOfRefraction;
+            }
+            else if (oneMedia > twoMedia)
+            {
+                double totalIncides = twoMedia / oneMedia;
+                if (angleOfInc > Math.Asin(totalIncides))
+                {
+                    angleOfRefraction = 0;
+                }
+                else
+                    angleOfRefraction = Math.Sin(angleOfInc) * indicesOfRefraction;
+            }
+            return angleOfRefraction;
+        }
+    }
+}
